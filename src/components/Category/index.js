f0e038a7691ef31e28ac7486984b2b589fddbb11
CarlosDummy14 <carlosdummy14@gmail.com>
2020-06-03 23:16:52 -0500

    initial commit NF - react

---
+++ src/components/Category/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Anchor, Img } from './styles'
+
+const DEFAULT_IMAGE = 'https://img.scryfall.com/cards/art_crop/front/1/c/1ca7065e-88c1-44bb-ac68-e6e1df9e0726.jpg?1590879855'
+
+export const Category = ({ cover = DEFAULT_IMAGE, path }) => {
+  return (
+    <Anchor href={path}>
+      <Img src={cover} alt='image' />
+      <p>Creature</p>
+    </Anchor>
+  )
+}
