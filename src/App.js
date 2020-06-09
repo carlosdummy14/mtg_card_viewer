9865499cdfe3ed0abbcfa0cb3e7480974f2ad2de
CarlosDummy14 <carlosdummy14@gmail.com>
2020-06-09 18:26:09 -0500

    PhotoCard animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GlobalStyles } from './GlobalStyles'
+import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { ListOfCategory } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
