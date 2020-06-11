36d408002512dd9febe1e3f602c4085d1ae37360
CarlosDummy14 <carlosdummy14@gmail.com>
2020-06-11 18:30:09 -0500

    PhotoCard IntersectionObserver feature

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { MdFavoriteBorder } from 'react-icons/md'
-import { Img, ImgWrapper, Button } from './styles'
+import { Article, Img, ImgWrapper, Button } from './styles'
 
 const DEFAULT_IMAGE = 'https://img.scryfall.com/cards/art_crop/front/1/c/1ca7065e-88c1-44bb-ac68-e6e1df9e0726.jpg?1590879855'
 
 export const PhotoCard = ({ id = '1', name = 'name', likes = '2', cover = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(entries => {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
+    })
+  }, [element])
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={cover} alt={cover} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' />{likes} likes!
-      </Button>
-      <p>{id}</p>
-      <p>{name}</p>
-    </article>
+    <Article ref={element}>
+      {
+        show &&
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={cover} alt={cover} />
+              </ImgWrapper>
+            </a>
+            <Button>
+              <MdFavoriteBorder size='32px' />{likes} likes!
+            </Button>
+            <p>{id}</p>
+            <p>{name}</p>
+          </>
+      }
+    </Article>
   )
 }
